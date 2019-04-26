bd8336d6a1b940c3d4f821ef65128557b61077c5
Sven Kube <kube@sipgate.de>
2019-04-26 10:09:40 +0200

    style: apply PEP8 styling conventions

--- sendsms.py
+++ sendsms.py
@@ -1,42 +1,43 @@
 import requests
-import datetime
+# Only needed when sending a scheduled sms at a specific date and time
+# import datetime
 
 
-def main():
-	baseURL = 'https://api.sipgate.com/v2'
-	username = 'YOUR_SIPGATE_EMAIL'
-	password = 'YOUR_SIPGATE_PASSWORD'
+def send_sms():
+    base_url = 'https://api.sipgate.com/v2'
+    username = 'YOUR_SIPGATE_EMAIL'
+    password = 'YOUR_SIPGATE_PASSWORD'
 
-	smsId = 'YOUR_SIPGATE_SMS_EXTENSION'
+    sms_id = 'YOUR_SIPGATE_SMS_EXTENSION'
 
-	message = 'YOUR_MESSAGE'
-	recipient = 'RECIPIENT_PHONE_NUMBER'
+    message = 'YOUR_MESSAGE'
+    recipient = 'RECIPIENT_PHONE_NUMBER'
 
-	# Only needed when sending a scheduled sms at a specific date and time
-	# sendAt = datetime.datetime(day=9, month=4, year=2019, hour=15, minute=35)
+    # Only needed when sending a scheduled sms at a specific date and time
+    # send_at = datetime.datetime(day=9, month=4, year=2019, hour=15, minute=35)
 
-	headers = {
-		'Content-Type': 'application/json'
-	}
+    headers = {
+        'Content-Type': 'application/json'
+    }
 
-	requestBody = {
-		'smsId': smsId,
-		'recipient': recipient,
-		'message': message,
-		# Only needed when sending a scheduled sms at a specific date and time
-		# 'sendAt': sendAt.timestamp()
-	}
+    request_body = {
+        'smsId': sms_id,
+        'recipient': recipient,
+        'message': message,
+        # Only needed when sending a scheduled sms at a specific date and time
+        # 'sendAt': send_at.timestamp()
+    }
 
-	response = requests.post(
-		f'{baseURL}/sessions/sms',
-		headers=headers,
-		auth=requests.auth.HTTPBasicAuth(username, password),
-		json=requestBody
-	)
+    response = requests.post(
+        base_url + '/sessions/sms',
+        headers=headers,
+        auth=requests.auth.HTTPBasicAuth(username, password),
+        json=request_body
+    )
 
-	print(f'Status: {response.status_code}')
-	print(f'Body: {response.content.decode("utf-8")}')
+    print('Status:', response.status_code)
+    print('Body:', response.content.decode("utf-8"))
 
 
 if __name__ == "__main__":
-	main()
+    send_sms()
